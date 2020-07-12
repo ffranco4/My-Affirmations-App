0cd739b03a1ecc2bd348ea5c9de3b2d68cd0f9f2
ffranco4 <52253626+ffranco4@users.noreply.github.com>
2020-07-12 12:10:41 -0500

    Initial Commit

---
+++ My Affirmations App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Affirmations App
+//
+//  Created by Student on 7/12/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
